028b185c5580df78010be33d58add7000503cc5b
Richard Yin <richard.hongyi.yin@gmail.com>
2015-07-15 22:39:26 -0400

    Update gitignore, finish setOccupant

--- src/io/github/richardyin/chess/ChessGridPanel.java
+++ src/io/github/richardyin/chess/ChessGridPanel.java
@@ -7,7 +7,6 @@ import ictk.boardgame.chess.Square;
 import ictk.boardgame.chess.ui.ChessBoardDisplay;
 import ictk.boardgame.ui.BoardDisplay;
 
-import java.awt.Color;
 import java.awt.Font;
 import java.awt.FontFormatException;
 import java.io.File;
@@ -35,10 +34,11 @@ public class ChessGridPanel extends JPanel implements ChessBoardDisplay,
 				for (int y = 0; y < 8; y++) {
 					buttonGrid[x][y] = new GridButton();
 					buttonGrid[x][y].setBounds(60 * x, 60 * y, 60, 60);
-					if (x % 2 == y % 2)
-						buttonGrid[x][y].setBackground(Color.WHITE);
-					else
-						buttonGrid[x][y].setBackground(Color.BLACK);
+					if (x % 2 == y % 2) {
+						buttonGrid[x][y].setBlack(false);
+					} else {
+						buttonGrid[x][y].setBlack(true);
+					}
 					buttonGrid[x][y].setFont(chessFont);
 					add(buttonGrid[x][y]);
 				}
@@ -49,6 +49,31 @@ public class ChessGridPanel extends JPanel implements ChessBoardDisplay,
 					"Error", JOptionPane.ERROR_MESSAGE);
 			throw e;
 		}
+	}
+
+	@Override
+	public Board getBoard() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setBoard(Board board) {
+		if (board instanceof ChessBoard) {
+			ChessBoard chessBoard = (ChessBoard) board;
+			for (int x = 1; x <= 8; x++) {
+				for (int y = 1; y <= 8; y++) {
+					Square sq = chessBoard.getSquare(x, y);
+					buttonGrid[x][y].setOccupant(sq.getOccupant());
+				}
+			}
+		} else
+			throw new IllegalArgumentException("Wrong type of board given");
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
 	}
 
 	@Override
@@ -95,30 +120,5 @@ public class ChessGridPanel extends JPanel implements ChessBoardDisplay,
 	@Override
 	public void boardUpdate(Board board, int updateType) {
 
-	}
-
-	@Override
-	public Board getBoard() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void setBoard(Board board) {
-		if (board instanceof ChessBoard) {
-			ChessBoard chessBoard = (ChessBoard) board;
-			for (int x = 1; x <= 8; x++) {
-				for (int y = 1; y <= 8; y++) {
-					Square sq = chessBoard.getSquare(x, y);
-					buttonGrid[x][y].setOccupant(sq.getOccupant());
-				}
-			}
-		} else
-			throw new IllegalArgumentException("Wrong type of board given");
-	}
-
-	@Override
-	public void update() {
-		// TODO Auto-generated method stub
 	}
 }
--- src/io/github/richardyin/chess/GridButton.java
+++ src/io/github/richardyin/chess/GridButton.java
@@ -1,11 +1,46 @@
 package io.github.richardyin.chess;
 
+import ictk.boardgame.chess.Bishop;
 import ictk.boardgame.chess.ChessPiece;
+import ictk.boardgame.chess.King;
+import ictk.boardgame.chess.Knight;
+import ictk.boardgame.chess.Pawn;
+import ictk.boardgame.chess.Queen;
+import ictk.boardgame.chess.Rook;
+
+import java.awt.Color;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 import javax.swing.JButton;
 
 public class GridButton extends JButton {
 	private ChessPiece occupant;
+	private boolean isBlack;
+
+	// Display an opaque ("black" in the font) piece if occupant colour is
+	// different from square colour; use this map to convert from Piece object
+	// to font
+	private static Map<PieceData, Character> referenceMap;
+
+	static {
+		byte[] indices = { Pawn.INDEX, Bishop.INDEX, Knight.INDEX, Rook.INDEX,
+				Queen.INDEX, King.INDEX }; // indices in the order that appears
+											// in the font
+		Map<PieceData, Character> charMap = new HashMap<>();
+		// White pieces are 'i' to 'n' while black pieces are 'I' to 'N'
+		char currentCharacter = 'I'; // different-colour pawn
+		char charDifference = (char) ('A' - 'a'); // diff btwn capital,
+													// lowercase
+		for (byte index : indices) {
+			charMap.put(new PieceData(index, false), currentCharacter);
+			charMap.put(new PieceData(index, true),
+					(char) (currentCharacter + charDifference));
+			currentCharacter++;
+		}
+		referenceMap = Collections.unmodifiableMap(charMap);
+	}
 
 	public GridButton() {
 
@@ -17,5 +52,50 @@ public class GridButton extends JButton {
 
 	public void setOccupant(ChessPiece occupant) {
 		this.occupant = occupant;
+		PieceData occupantData = new PieceData(occupant.getIndex(),
+				(occupant.isBlack() == isBlack));
+		setText(referenceMap.get(occupantData).toString());
+	}
+
+	public boolean isBlack() {
+		return isBlack;
+	}
+
+	public void setBlack(boolean isBlack) {
+		this.isBlack = isBlack;
+		if (isBlack) {
+			setBackground(Color.BLACK);
+			setForeground(Color.WHITE);
+		} else {
+			setBackground(Color.WHITE);
+			setBackground(Color.BLACK);
+		}
+	}
+
+	private static class PieceData {
+		private boolean sameColour;
+		private byte index; // Refer to <PieceClassName>.INDEX
+
+		public PieceData(byte index, boolean sameColour) {
+			this.sameColour = sameColour;
+			this.index = index;
+		}
+
+		// Piece data with the same type and colour are equal
+		@Override
+		public int hashCode() {
+			int code = index;
+			if (sameColour)
+				code += 8; // indices are from 0 to 5
+			return code;
+		}
+
+		@Override
+		public boolean equals(Object o) {
+			if (!(o instanceof PieceData))
+				return false;
+			PieceData data = (PieceData) o;
+			return data.index == index && data.index == index;
+		}
 	}
 }
